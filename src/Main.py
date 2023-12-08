ebea241a2d5a4d1d33cfd6132268273a19fabbb1
julianfisla <jfislayt@gmail.com>
2023-12-08 17:43:18 -0500

    Comments.

--- src/Main.py
+++ src/Main.py
@@ -17,6 +17,7 @@ fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 800
 
+# Create the window
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('2D Fighter')
 
